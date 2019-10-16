369603727518821bf4a2d24d1885750b28f3d3ac
Simon Agius Muscat <simon@agius-muscat.net>
2019-10-16 23:10:41 +0200

    Added loading go example

--- benchmarking/prime/prime.go
+++ benchmarking/prime/prime.go
@@ -1,4 +1,27 @@
-package prime
+package main
+
+// References:
+// https://tutorialedge.net/golang/go-webassembly-tutorial/
+
+import (
+	"syscall/js"
+)
+
+func main() {
+	c := make(chan bool)
+	println("WASM Go Initialized")
+	registerCallbacks()
+	<-c
+}
+
+func add(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+}
 
 // Prime - generates all prime numbers up to N
 // https://stackoverflow.com/questions/21854191/generating-prime-numbers-in-go
