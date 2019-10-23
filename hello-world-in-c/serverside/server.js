bdc5eb5f954e642faf407550ea2cae0fc017b2a2
Simon Agius Muscat <simon@agius-muscat.net>
2019-10-23 18:46:27 +0200

    Cleaned up hello world client side example

---
+++ hello-world-in-c/serverside/server.js
@@ -0,0 +1,39 @@
+// References:
+// https://dev.to/azure/experimenting-with-web-assembly-and-nodejs-40f4
+// https://www.codepool.biz/use-webassembly-node-js.html
+// https://gist.github.com/kripken/59c67556dc03bb6d57052fedef1e61ab
+
+const fs = require('fs');
+var source = fs.readFileSync('./add.wasm');
+
+var typedArray = new Uint8Array(source);
+
+const env = {
+  abortStackOverflow: () => {
+    throw new Error('overflow');
+  },
+  __memory_base: 1024,
+  __table_base: 0,
+  memory: new WebAssembly.Memory({
+    initial: 256,
+    maximum: 256
+  }),
+  table: new WebAssembly.Table({
+    initial: 0,
+    maximum: 0,
+    element: 'anyfunc'
+  }),
+  STACKOP: 0
+};
+
+WebAssembly.instantiate(typedArray, {
+  env: env
+})
+  .then(result => {
+    const add = result.instance.exports._add;
+    console.log(add(9, 9));
+  })
+  .catch(e => {
+    // error caught
+    console.log(e);
+  });
---
+++ hello-world/clientside/README.md
@@ -0,0 +1,17 @@
+# Hello World (client-side)
+
+This example allows us to run the already compiled wasm file in the browser.
+
+## Pre-requisites
+
+[Emscripten SDK](https://github.com/emscripten-core/emsdk)
+
+## Running
+
+To run, simply run the following command:
+
+`make`
+
+Then access the file `hello.html` at `http://localhost:8000`.
+
+If you are not on a Linx/Mac system, look into the Makefile to see the exact command that needs to be run.
